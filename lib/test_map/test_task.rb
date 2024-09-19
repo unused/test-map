e972cd51b1e625483fbe00d0e5be29e3c3d07696
Christoph Lipautz <christoph@lipautz.org>
2024-09-20 00:25:10 +0200

    Run changed and mapped test files

--- lib/test_map/test_task.rb
+++ lib/test_map/test_task.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative 'mapping'
+require 'rake/testtask'
+require 'minitest'
+require 'minitest/unit'
 
 module TestMap
   # TestTask is a rake helper class.
@@ -16,9 +19,12 @@ module TestMap
         desc 'Run tests for changed files'
         task :changes do
           out_file = "#{Dir.pwd}/.test-map.yml"
-          test_files = Mapping.new(out_file).lookup(ARGV[1..])
-          # TODO: works with Rails, not in general
-          Rake::Task[@name].invoke test_files
+          test_files = Mapping.new(out_file).lookup(ARGV[1..]).compact
+
+          # puts "Running tests #{test_files.join(' ')}"
+          task = Minitest::TestTask.new
+          task.test_globs = test_files
+          ruby task.make_test_cmd
         end
       end
     end
