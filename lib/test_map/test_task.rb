a70c4b51bf158e5c8d584711b744d02776de7f9f
Christoph Lipautz <christoph@lipautz.org>
2024-09-20 00:38:17 +0200

    Use adapters for test tasks

--- lib/test_map/test_task.rb
+++ lib/test_map/test_task.rb
@@ -8,8 +8,27 @@ require 'minitest/unit'
 module TestMap
   # TestTask is a rake helper class.
   class TestTask < Rake::TaskLib
+    # Error for unknown test task adapter.
+    class UnknownAdapterError < StandardError; end
+
     def initialize(name) # rubocop:disable Lint/MissingSuper
       @name = name
+    end
+
+    # Adapter for minitest test task.
+    class MinitestTask < Minitest::TestTask
+      def call = ruby(make_test_cmd)
+
+      def files=(test_files)
+        self.test_globs = test_files
+      end
+    end
+
+    # Adapter for rspec test task
+    class RSpecTask
+      attr_accessor :files
+
+      def call = `rspec #{files.join(' ')}`
     end
 
     def self.create(name = :test) = new(name).define
@@ -22,11 +41,22 @@ module TestMap
           test_files = Mapping.new(out_file).lookup(ARGV[1..]).compact
 
           # puts "Running tests #{test_files.join(' ')}"
-          task = Minitest::TestTask.new
-          task.test_globs = test_files
-          ruby task.make_test_cmd
+          test_task.files = test_files
+          test_task.call
         end
       end
+    end
+
+    def test_task = @test_task ||= build_test_task
+
+    def build_test_task
+      if defined?(Minitest)
+        return MinitestTask.new
+      elsif defined?(RSpec)
+        return RSpecTask.new
+      end
+
+      raise UnknownAdapterError, 'No test task adapter found'
     end
   end
 end
