29691f603329b87bdfd72d31c46810d3110ddedb
Christoph Lipautz <christoph@lipautz.org>
2024-10-25 21:19:19 +0200

    Create specific test handler for Rails

--- lib/test_map/test_task.rb
+++ lib/test_map/test_task.rb
@@ -15,6 +15,13 @@ module TestMap
       @name = name
     end
 
+    # Adapter for rspec test task
+    class RailsTestTask
+      attr_accessor :files
+
+      def call = Rails::TestUnit::Runner.run_from_rake('test', files)
+    end
+
     # Adapter for minitest test task.
     class MinitestTask < Minitest::TestTask
       def call = ruby(make_test_cmd, verbose: false)
@@ -38,7 +45,8 @@ module TestMap
         desc 'Run tests for changed files'
         task :changes do
           out_file = "#{Dir.pwd}/.test-map.yml"
-          test_files = Mapping.new(out_file).lookup(*ARGV[1..])
+          args = defined?(Rails) ? ENV['TEST']&.split : ARGV[1..]
+          test_files = Mapping.new(out_file).lookup(*args)
 
           # puts "Running tests #{test_files.join(' ')}"
           test_task.files = test_files
@@ -50,7 +58,10 @@ module TestMap
     def test_task = @test_task ||= build_test_task
 
     def build_test_task
-      if defined?(Minitest)
+      if defined?(Rails)
+        return RailsTestTask.new
+      elsif defined?(Minitest)
+        require 'minitest/test_task'
         return MinitestTask.new
       elsif defined?(RSpec)
         return RSpecTask.new
